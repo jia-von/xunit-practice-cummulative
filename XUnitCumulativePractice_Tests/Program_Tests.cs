4fa79e2286be3b5b30282623d0151905b9ec9b19
Jia <jiavon@ualberta.ca>
2020-09-11 10:13:34 -0600

    initial commit

---
+++ XUnitCumulativePractice_Tests/Program_Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitCumulativePractice_Tests
+{
+    public class Program_Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
