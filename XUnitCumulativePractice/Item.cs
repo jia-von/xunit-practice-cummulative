00e781f8100eaeff67627535f3b9462f5c424bd5
Jia <jiavon@ualberta.ca>
2020-09-11 11:25:34 -0600

    storage class and item class completed

---
+++ XUnitCumulativePractice/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitCumulativePractice
+{
+    public class Item //Create a class called “Item” with the following properties:
+    {
+        public string Name { get; set; } //“Name” (string).
+
+        //A default and greedy constructor.
+        public Item()
+        {
+            Name = "Default Name";
+        }
+
+        public Item(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ XUnitCumulativePractice/Storage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitCumulativePractice //Create a class called “Storage” with the following properties and methods:
+{
+    public class Storage
+    {
+        public List<Item> Contents { get; set; }//“Contents” (list of Items)
+
+        public void AddItem(Item item)//AddItem() will accept an item and add it to the “Contents” list.
+        {
+            Contents.Add(item);
+        }
+
+        public void RemoveItem()
+        {
+            Contents.RemoveAt(Contents.Count - 1); //James Grieve, 9-Sep-2020 
+        }
+
+        public Storage()
+        {
+            Contents = new List<Item>();
+        }
+    }
+}
